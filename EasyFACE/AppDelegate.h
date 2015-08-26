442b2bd8e124d43e17bbbcfdbeded0262f4e004f
Mr.NutEgg <542272232@qq.com>
2015-08-26 22:01:38 +0800

    Initial Commit

---
+++ EasyFACE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyFACE
+//
+//  Created by wosinC on 15/8/26.
+//  Copyright (c) 2015年 wosinC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
